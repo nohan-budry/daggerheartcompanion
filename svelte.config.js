dc3581af23f05e90ab21e99b0cba78f9e6a232b0
Nohan Budry <nohan.budry@icloud.com>
2025-07-06 16:26:54 +0200

    update github pages action

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
 import {vitePreprocess} from '@sveltejs/vite-plugin-svelte';
-
 import classesData from './src/lib/data/classes.json' with {type: "json"};
 
 
@@ -16,7 +15,6 @@ const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -24,7 +22,10 @@ const config = {
     adapter: adapter(),
     prerender: {
       entries: prerenderEntries,
-    }
+    },
+    paths: {
+      base: process.env.PUBLIC_APP_PATH ?? '',
+    },
   }
 };
 
