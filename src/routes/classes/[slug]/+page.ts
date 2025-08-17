ab323ae430896593b6bea1da895d08f9ee39223f
Nohan Budry <nohan.budry@icloud.com>
2025-08-17 07:38:02 +0200

    1: remove old data in lib and remove entries in pages with slugs

--- src/routes/classes/[slug]/+page.ts
+++
@@ -1,9 +0,0 @@
-import type {EntryGenerator} from "./$types";
-import rawClasses from '$lib/data/classes.json';
-import type ClassData from "$lib/models/ClassData";
-import {slugify} from "$lib/utils/slugify";
-
-export const entries: EntryGenerator = async () => {
-    return (rawClasses as ClassData[])
-        .map(({name}) => ({slug: slugify(name)}));
-}
